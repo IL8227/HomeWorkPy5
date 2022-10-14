17a2bc1da3925ce9592c23826d139adca68b8ef0
IliaV <iliaspb2012@gmail.com>
2022-10-14 17:48:37 +0300

    task2

--- Task2.py
+++ Task2.py
@@ -1,6 +1,17 @@
-from random import randint
+# Создайте программу для игры с конфетами человек против человека.
 
-def input_dat(name):
+# Условие задачи: На столе лежит 2021 конфета. Играют два игрока делая ход друг после друга. Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет. Все конфеты оппонента достаются сделавшему последний ход. Сколько конфет нужно взять первому игроку, чтобы забрать все конфеты у своего конкурента?
+
+# a) Добавьте игру против бота
+
+# b) Подумайте как наделить бота ""интеллектом""
+
+
+from os import fwalk
+from random import randint
+from tkinter import W
+
+def input_(name):
     x = int(input(f"{name}, введите количество конфет, которое возьмете от 1 до 28: "))
     while x < 1 or x > 28:
         x = int(input(f"{name}, введите корректное количество конфет: "))
@@ -8,12 +19,12 @@ def input_dat(name):
 
 
 def p_print(name, k, counter, value):
-    print(f'Ходил {name}, он взял {k}, теперь у него {counter}. Осталось на столе {value} конфет.')
+    print(f"Ходил {name}, он взял {k}, теперь у него {counter}. Осталось на столе {value} конфет.")
 
-player1 = input('Введите имя первого игрока: ')
-player2 = input('Введите имя второго игрока: ')
-value = int(input('Введите количество конфет на столе: '))
-f = randint(0,2) 
+player1 = input("Введите имя первого игрока: ")
+player2 = input("Введите имя второго игрока: ")
+value = int(input("Введите количество конфет на столе: "))
+f = randint(0,2)
 if f:
     print(f"Первый ходит {player1}")
 else:
@@ -24,13 +35,13 @@ counter2 = 0
 
 while value > 28:
     if f:
-        k = input_dat(player1)
+        k = input_(player1)
         counter1 += k
         value -= k
-        f = False
+        flag = False
         p_print(player1, k, counter1, value)
     else:
-        k = input_dat(player2)
+        k = input_(player2)
         counter2 += k
         value -= k
         f = True
